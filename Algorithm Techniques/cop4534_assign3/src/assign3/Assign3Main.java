6ef250d7ea3c9610960f5d06cfebad01c0b66d30
adammerille <abmerille@gmail.com>
2014-10-03 12:55:14 -0400

    added assignment 3 folder

---
+++ Algorithm Techniques/cop4534_assign3/src/assign3/Assign3Main.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package assign3;
+
+/**
+ * use adjaceny matrix
+ * start with no edges (only edges out of s and in to t)
+ *for k=1 .. N
+ * {
+ *  add edge to residualgraph where one side k and other <=k
+ *  find flow, increase flow
+ *  if(flow ==N done)
+ * }
+ * @author AdamM
+ */
+public class Assign3Main{
+    //adjacency list of persons where each person can be passed a name that is hashed (bucketsize = N) to rank
+    
+    public static void main(String[] args){
+        
+    }
+}
