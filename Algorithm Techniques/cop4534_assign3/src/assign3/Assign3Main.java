e6337b34c4d48e1567a63e914a21f75d156cce83
Adam <Adam@AdamYoga>
2014-10-05 13:32:14 -0400

    Added Node class and method to get cmdArgs

--- Algorithm Techniques/cop4534_assign3/src/assign3/Assign3Main.java
+++ Algorithm Techniques/cop4534_assign3/src/assign3/Assign3Main.java
@@ -5,6 +5,9 @@
  */
 package assign3;
 
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  * use adjaceny matrix
  * start with no edges (only edges out of s and in to t)
@@ -18,8 +21,31 @@ package assign3;
  */
 public class Assign3Main{
     //adjacency list of persons where each person can be passed a name that is hashed (bucketsize = N) to rank
+    public static List<String> files;
     
-    public static void main(String[] args){
+    public static void main(String[] args)
+    {
+        files = getArgs(args);
         
+        for(int i =0; i < files.size(); i++)
+        {
+            
+        }
+        
+    }
+    
+    public static LinkedList<String> getArgs(String[] args)
+    {
+        LinkedList<String> argList = new LinkedList<>();
+        String arg = args[0];
+        if(arg.contains(".txt"))
+        {
+            argList.add(arg);
+        }
+        else
+        {
+            System.err.println("Error: input files should end in .txt");
+        }
+        return argList;
     }
 }
